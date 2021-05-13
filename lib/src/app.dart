b30e6076310a25c83959f1818c3152c54c4825ae
Fabrizio Nancay P <elsertero56789@gmail.com>
2021-05-13 00:03:09 -0500

    Agregando Proyecto

---
+++ lib/src/app.dart
@@ -0,0 +1,9 @@
+import 'package:contador/src/home_page.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return MaterialApp(home: Center(child: HomePage()));
+  }
+}
